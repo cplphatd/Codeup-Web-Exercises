bf008834ac0b906515d40e8ce09241e37e4268d0
David Ryan Alviola <david.alviola@gmail.com>
2016-11-16 16:30:33 -0600

    Completed calculator project. Added running total functionality and clear button functionality.  Reduced code by using one function to handle all button presses for each operand and operator.

--- js/calculator.js
+++ js/calculator.js
@@ -5,16 +5,34 @@
 (function () {
 
     var btnOne = document.getElementById("btnOne");
+    var btnTwo = document.getElementById("btnTwo");
+    var btnThree = document.getElementById("btnThree");
+    var btnFour = document.getElementById("btnFour");
+    var btnFive = document.getElementById("btnFive");
+    var btnSix = document.getElementById("btnSix");
+    var btnSeven = document.getElementById("btnSeven");
+    var btnEight = document.getElementById("btnEight");
+    var btnNine = document.getElementById("btnNine");
+    var btnZero = document.getElementById("btnZero");
     var btnPlus = document.getElementById("btnPlus");
+    var btnMinus = document.getElementById("btnMinus");
+    var btnMultiply = document.getElementById("btnMultiply");
+    var btnDivide = document.getElementById("btnDivide");
+    var btnClear = document.getElementById("btnClear");
     var btnEquals = document.getElementById("btnEquals");
-    var displayedLeftOperand = document.getElementById("leftOperand");
-    var displayedLeftOperandValue = displayedLeftOperand.getAttribute("value");
-    var leftOperandValueForMath;
-    var displayedRightOperand = document.getElementById("rightOperand");
-    var displayedRightOperandValue = displayedRightOperand.getAttribute("value");
-    var rightOperandValueForMath;
+    var operatorDisplay = document.getElementById("operator");
+    var displayedLeftOperand = document.getElementById("leftOperand");              //The actual left operand element
+    var displayedLeftOperandValue = displayedLeftOperand.getAttribute("value");     //The value of the left operand element
+    var leftOperandValueForMath = 0;                                                //Value of left operand converted to num for math
+    var displayedRightOperand = document.getElementById("rightOperand");            //The actual right operand element
+    var displayedRightOperandValue = 0;                                             //The value of the right operand element
+    var rightOperandValueForMath = 1;                                               //Value of right operand converted to num for math
     var operator;
     var total;
+    var leftOperandBtnPress;
+    var rightOperandBtnPress;
+    var operatorBtnPress;
+    var msg = document.getElementById("msg");
 
     function convertOperandToString (displayedOperandValue) {
         displayedOperandValue.toString();
@@ -26,61 +44,182 @@
     }
 
     //All numerical button press functions will follow this structure
-    function pressBtnOneLeftOperand () {
+
+    leftOperandBtnPress = function () {
         if (displayedLeftOperandValue > 0) {
-            displayedLeftOperand.setAttribute("value", displayedLeftOperandValue + 1);
+            displayedLeftOperand.setAttribute("value", displayedLeftOperandValue + this.innerHTML);
             displayedLeftOperandValue = displayedLeftOperand.getAttribute("value");
             displayedLeftOperandValue = convertOperandToString(displayedLeftOperandValue);
+            leftOperandValueForMath = convertOperandToNumber(displayedLeftOperandValue);
         } else {
-            displayedLeftOperand.setAttribute("value", 1);
+            displayedLeftOperand.setAttribute("value", this.innerHTML);
             displayedLeftOperandValue = displayedLeftOperand.getAttribute("value");
             displayedLeftOperandValue = convertOperandToString(displayedLeftOperandValue);
+            leftOperandValueForMath = convertOperandToNumber(displayedLeftOperandValue);
         }
     }
 
-    function pressBtnOneRightOperand () {
+    rightOperandBtnPress = function () {
         if (displayedRightOperandValue > 0) {
-            displayedRightOperand.setAttribute("value", displayedRightOperandValue + 1);
+            displayedRightOperand.setAttribute("value", displayedRightOperandValue + this.innerHTML);
             displayedRightOperandValue = displayedRightOperand.getAttribute("value");
             convertOperandToString(displayedRightOperandValue);
+            rightOperandValueForMath = convertOperandToNumber(displayedRightOperandValue);
         } else {
-            displayedRightOperand.setAttribute("value", 1);
+            displayedRightOperand.setAttribute("value", this.innerHTML);
             displayedRightOperandValue = displayedRightOperand.getAttribute("value");
             convertOperandToString(displayedRightOperandValue);
+            rightOperandValueForMath = convertOperandToNumber(displayedRightOperandValue);
         }
     }
 
     //all operator button press functions will follow this structure
-    function pressPlusBtn () {
-        operator = "+";
-        leftOperandValueForMath = convertOperandToNumber(displayedLeftOperandValue);
-        console.log(leftOperandValueForMath);
-        document.getElementById("operator").setAttribute("value", "+");
+
+    operatorBtnPress = function () {
+        calculate();
+        operator = this.innerHTML.toString();
+        operatorDisplay.setAttribute("value", this.innerHTML);
         enterRightOperand();
     }
 
     function enterLeftOperand () {
-        btnOne.addEventListener("click", pressBtnOneLeftOperand);
-        btnPlus.addEventListener("click", pressPlusBtn);
-        btnOne.removeEventListener("click", pressBtnOneRightOperand);
+        btnOne.addEventListener("click", leftOperandBtnPress);
+        btnTwo.addEventListener("click", leftOperandBtnPress);
+        btnThree.addEventListener("click", leftOperandBtnPress);
+        btnFour.addEventListener("click", leftOperandBtnPress);
+        btnFive.addEventListener("click", leftOperandBtnPress);
+        btnSix.addEventListener("click", leftOperandBtnPress);
+        btnSeven.addEventListener("click", leftOperandBtnPress);
+        btnEight.addEventListener("click", leftOperandBtnPress);
+        btnNine.addEventListener("click", leftOperandBtnPress);
+        btnZero.addEventListener("click", leftOperandBtnPress);
+        btnPlus.addEventListener("click", operatorBtnPress);
+        btnMinus.addEventListener("click", operatorBtnPress);
+        btnMultiply.addEventListener("click", operatorBtnPress);
+        btnDivide.addEventListener("click", operatorBtnPress);
+        btnOne.removeEventListener("click", rightOperandBtnPress);
+        btnTwo.removeEventListener("click", rightOperandBtnPress);
+        btnThree.removeEventListener("click", rightOperandBtnPress);
+        btnFour.removeEventListener("click", rightOperandBtnPress);
+        btnFive.removeEventListener("click", rightOperandBtnPress);
+        btnSix.removeEventListener("click", rightOperandBtnPress);
+        btnSeven.removeEventListener("click", rightOperandBtnPress);
+        btnEight.removeEventListener("click", rightOperandBtnPress);
+        btnNine.removeEventListener("click", rightOperandBtnPress);
+        btnZero.removeEventListener("click", rightOperandBtnPress);
+        btnClear.addEventListener("click", pressClear);
     }
 
     function enterRightOperand () {
-        btnOne.removeEventListener("click", pressBtnOneLeftOperand);
-        btnOne.addEventListener("click", pressBtnOneRightOperand);
-        btnEquals.addEventListener("click", calculate);
+        displayedLeftOperand.setAttribute("value", displayedLeftOperandValue);
+        displayedRightOperand.setAttribute("value", "");
+        displayedRightOperandValue = 0;
+        btnOne.removeEventListener("click", leftOperandBtnPress);
+        btnTwo.removeEventListener("click", leftOperandBtnPress);
+        btnThree.removeEventListener("click", leftOperandBtnPress);
+        btnFour.removeEventListener("click", leftOperandBtnPress);
+        btnFive.removeEventListener("click", leftOperandBtnPress);
+        btnSix.removeEventListener("click", leftOperandBtnPress);
+        btnSeven.removeEventListener("click", leftOperandBtnPress);
+        btnEight.removeEventListener("click", leftOperandBtnPress);
+        btnNine.removeEventListener("click", leftOperandBtnPress);
+        btnZero.removeEventListener("click", leftOperandBtnPress);
+        btnOne.addEventListener("click", rightOperandBtnPress);
+        btnTwo.addEventListener("click", rightOperandBtnPress);
+        btnThree.addEventListener("click", rightOperandBtnPress);
+        btnFour.addEventListener("click", rightOperandBtnPress);
+        btnFive.addEventListener("click", rightOperandBtnPress);
+        btnSix.addEventListener("click", rightOperandBtnPress);
+        btnSeven.addEventListener("click", rightOperandBtnPress);
+        btnEight.addEventListener("click", rightOperandBtnPress);
+        btnNine.addEventListener("click", rightOperandBtnPress);
+        btnZero.addEventListener("click", rightOperandBtnPress);
+        btnEquals.addEventListener("click", pressEquals);
     }
 
     function calculate () {
-        rightOperandValueForMath = convertOperandToNumber(displayedRightOperandValue);
         switch (operator) {
             case "+" :
                 total = leftOperandValueForMath + rightOperandValueForMath;
+                displayedLeftOperandValue = total;
                 leftOperandValueForMath = total;
-                console.log(leftOperandValueForMath);
-                enterLeftOperand ();
+                enterRightOperand ();
                 break;
+            case "-" :
+                total = leftOperandValueForMath - rightOperandValueForMath;
+                displayedLeftOperandValue = total;
+                leftOperandValueForMath = total;
+                enterRightOperand ();
+                break;
+            case "*" :
+                total = leftOperandValueForMath * rightOperandValueForMath;
+                displayedLeftOperandValue = total;
+                leftOperandValueForMath = total;
+                enterRightOperand ();
+                break;
+            case "/" :
+                if (rightOperandValueForMath === 0) {
+                    msg.innerHTML = "Cannot divide by zero. Clear and try again."
+                    btnOne.removeEventListener("click", rightOperandBtnPress);
+                    btnTwo.removeEventListener("click", rightOperandBtnPress);
+                    btnThree.removeEventListener("click", rightOperandBtnPress);
+                    btnFour.removeEventListener("click", rightOperandBtnPress);
+                    btnFive.removeEventListener("click", rightOperandBtnPress);
+                    btnSix.removeEventListener("click", rightOperandBtnPress);
+                    btnSeven.removeEventListener("click", rightOperandBtnPress);
+                    btnEight.removeEventListener("click", rightOperandBtnPress);
+                    btnNine.removeEventListener("click", rightOperandBtnPress);
+                    btnZero.removeEventListener("click", rightOperandBtnPress);
+                    btnOne.removeEventListener("click", leftOperandBtnPress);
+                    btnTwo.removeEventListener("click", leftOperandBtnPress);
+                    btnThree.removeEventListener("click", leftOperandBtnPress);
+                    btnFour.removeEventListener("click", leftOperandBtnPress);
+                    btnFive.removeEventListener("click", leftOperandBtnPress);
+                    btnSix.removeEventListener("click", leftOperandBtnPress);
+                    btnSeven.removeEventListener("click", leftOperandBtnPress);
+                    btnEight.removeEventListener("click", leftOperandBtnPress);
+                    btnNine.removeEventListener("click", leftOperandBtnPress);
+                    btnZero.removeEventListener("click", leftOperandBtnPress);
+                    btnPlus.removeEventListener("click", operatorBtnPress);
+                    btnMinus.removeEventListener("click", operatorBtnPress);
+                    btnMultiply.removeEventListener("click", operatorBtnPress);
+                    btnDivide.removeEventListener("click", operatorBtnPress);
+                    btnEquals.removeEventListener("click", pressEquals);
+                } else {
+                    total = leftOperandValueForMath / rightOperandValueForMath;
+                    displayedLeftOperandValue = total;
+                    leftOperandValueForMath = total;
+                    enterRightOperand();
+                }
+                break;
+            default :
+                total = leftOperandValueForMath / rightOperandValueForMath;
+                displayedLeftOperandValue = total;
+                leftOperandValueForMath = total;
+                enterRightOperand ();
         }
+    }
+
+    function pressEquals () {
+        calculate();
+        operatorDisplay.setAttribute("value", "");
+        operator = "";
+        displayedRightOperand.setAttribute("value", "");
+        displayedRightOperandValue = 0;
+        rightOperandValueForMath = 1;
+    }
+
+    function pressClear () {
+        operatorDisplay.setAttribute("value", "");
+        operator = "";
+        displayedLeftOperand.setAttribute("value", 0);
+        displayedLeftOperandValue = 0;
+        leftOperandValueForMath = 0;
+        displayedRightOperand.setAttribute("value", "");
+        displayedRightOperandValue = 0;
+        rightOperandValueForMath = 1;
+        msg.innerHTML = "";
+        enterLeftOperand();
     }
 
     enterLeftOperand();
