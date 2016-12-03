0c3eefbc78da1bb34daf73c014139c4f434959c9
David Ryan Alviola <david.alviola@gmail.com>
2016-12-03 13:32:13 -0600

    Fixed bugs with add and remove post functionality for ajax blog.

--- js/ajax_blog.js
+++ js/ajax_blog.js
@@ -4,55 +4,48 @@
 "use strict";
 (function () {
 
-        //ajax request to retrieve posts
-        var getPosts = function () {
-            return $.get("data/blog.json");
-        };
+        //Array to hold blog contents
+        var blogArray = [];
 
         //Assignment of returned object to posts
-        var posts = getPosts();
+        var posts;
+
+        //ajax request to retrieve posts (IIFE)
+        var getPosts = (function () {
+            posts = $.get("data/blog.json");
+        }) ();
 
         //Function to generate the blog
         var writeBlog = function (blogContents) {
             blogContents.done(function (array) {
-                var collection = [];
                 array.forEach(function (object) {
-                    collection += "<div class='blog-post rounded'>"
+                    blogArray += "<div class='blog-post'>"
                         + "<h2 class='blog-post-title'>" + object.title + "</h2>"
                         + "<p class='blog-post-meta'>Date: " + object.date + "</p>"
                         + "<p>" + object.content + "</p>"
                         + "<p class='blog-post-meta'>Tags: " + object.categories.join(", ") + "</p>"
                         + "</div><hr>";
                 });
-                $("#posts").html(collection);
+                $("#posts").html(blogArray);
             });
         };
 
         //Function that generates the blog with the exception of the last indexed object
         var removeLastPost = function (blogContents) {
             blogContents.done(function (array) {
-                var collection = [];
-                array.forEach(function (object, index) {
-                    if (index != array.length - 1) {
-                        collection += "<div class='blog-post rounded'>"
-                            + "<h2 class='blog-post-title'>" + object.title + "</h2>"
-                            + "<p class='blog-post-meta'>Date: " + object.date + "</p>"
-                            + "<p>" + object.content + "</p>"
-                            + "<p class='blog-post-meta'>Tags: " + object.categories.join(", ") + "</p>"
-                            + "</div><hr>";
-                    }
-                });
-                $("#posts").html(collection);
+                blogArray = [];
+                array.pop();
+                $(".blog-post").last().remove();
             });
         };
 
         //Object to hold new post keys
-        var newPost = {};
+        var newPost = [{}];
 
-        //Function to get new post object and add to array json objects
+        //Function to get new post object and add to array of json objects
         var addPost = function (blogContents) {
             blogContents.done(function (array) {
-                var collection = [];
+                blogArray = [];
                 newPost.title = $("#postTitle").val();
                 newPost.date = $("#postDate").val();
                 newPost.content = $("#postBody").val();
@@ -60,16 +53,23 @@
                 newPost.categories = $("#postTags").val().split(",");
                 array.push(newPost);
                 console.log(array);
-                array.forEach(function (object, index) {
-                    collection += "<div class='blog-post rounded'>"
-                        + "<h2 class='blog-post-title'>" + object.title + "</h2>"
-                        + "<p class='blog-post-meta'>Date: " + object.date + "</p>"
-                        + "<p>" + object.content + "</p>"
-                        + "<p class='blog-post-meta'>Tags: " + object.categories.join(", ") + "</p>"
-                        + "</div><hr>";
-                });
-                $("#posts").html(collection);
+                clearAllInputs();
+                blogArray += "<div class='blog-post'>"
+                    + "<h2 class='blog-post-title'>" + newPost.title + "</h2>"
+                    + "<p class='blog-post-meta'>Date: " + newPost.date + "</p>"
+                    + "<p>" + newPost.content + "</p>"
+                    + "<p class='blog-post-meta'>Tags: " + newPost.categories.join(", ") + "</p>"
+                    + "</div><hr>";
+                $("#posts").append(blogArray);
             });
+        };
+
+        //Clears input fields
+        var clearAllInputs = function () {
+            $("#postTitle").val("");
+            $("#postDate").val("");
+            $("#postBody").val("");
+            $("#postTags").val("");
         };
 
         //Populates blog initially
@@ -77,13 +77,11 @@
 
         //Event handler to remove last blog that is stored in JSON
         $("#removeLast").click(function () {
-            posts = getPosts();
             removeLastPost(posts);
         });
 
         //Event handler to add new post from user
         $("#addNew").click(function () {
-            posts = getPosts();
             addPost(posts);
         });
 })();
