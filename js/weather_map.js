c9c88fedd120fa4691a78dd8f235bb1ccba041c5
David Ryan Alviola <david.alviola@gmail.com>
2016-12-06 10:03:52 -0600

    Added map from googlemaps api to weather map.

--- js/weather_map.js
+++ js/weather_map.js
@@ -23,20 +23,55 @@
     //Call getWeatherInfo by assigning to weatherInfo (any time weatherInfo is passed into a function, an ajax request is sent)
     weatherRequest = getWeatherInfo();
 
+    //Gets current time and stores in variable
+    var currentTime = new Date().getTime();
+
+    //Array that holds the three dates
+    var threeDayArray = [];
+
+    //Takes a starting time and creates three days
+    var generateThreeDates = function (startTime) {
+        var date;
+        for(var i = 0; i <= 2; i += 1) {
+            switch (i) {
+                case 0 :
+                    date = new Date(startTime);
+                    date.toString();
+                    threeDayArray.push(date);
+                    break;
+                case 1 :
+                    date = new Date(startTime + 8.64e+7);
+                    date.toString();
+                    threeDayArray.push(date);
+                    break;
+                case 2 :
+                    date = new Date(startTime + (8.64e+7 * 2));
+                    date.toString();
+                    threeDayArray.push(date);
+                    break;
+                default:
+                    console.log("error");
+            }
+        }
+    };
+
+    //Call date function
+    generateThreeDates(currentTime);
+
     //Write current weather conditions to html
     var postWeather = function (request) {
         request.done(function (weatherInfo) {
             $("#location").html("<h2>" + weatherInfo.city.name + "'s Three Day Forecast</h2>");
             console.log(weatherInfo);
-            for (var i = 0; i <= 2; i += 1) {
+            for (var i = 0; i <= threeDayArray.length - 1; i += 1) {
                 pageContents +=
-                    "<div class='box'><h5>High/Low</h5>"
-                    + "<h3>" + weatherInfo.list[i].temp.max + "/" + weatherInfo.list[i].temp.min + "°</h3>"
+                    "<div class='box'><h5>" + threeDayArray[i] + "</h5>"
+                    + "<h2>" + Math.round(weatherInfo.list[i].temp.max) + "/" + Math.round(weatherInfo.list[i].temp.min) + "°</h2>"
                     + "<div><img src='http://openweathermap.org/img/w/" + weatherInfo.list[i].weather[0].icon +".png'></div>"
-                    + "<h5>" + weatherInfo.list[i].weather[0].main + ": " + weatherInfo.list[i].weather[0].description + "</h5>"
-                    + "<h5>Humidity: " + weatherInfo.list[i].humidity + "%</h5>"
-                    + "<h5>Wind Speed: " + weatherInfo.list[i].speed + "mph</h5>"
-                    + "<h5>Pressure: " + weatherInfo.list[i].pressure + "mb</h5>"
+                    + "<h4>" + weatherInfo.list[i].weather[0].main + ": " + weatherInfo.list[i].weather[0].description + "</h4>"
+                    + "<h4>Humidity: " + weatherInfo.list[i].humidity + "%</h4>"
+                    + "<h4>Wind Speed: " + weatherInfo.list[i].speed + "mph</h4>"
+                    + "<h4>Pressure: " + weatherInfo.list[i].pressure + "mb</h4>"
                     + "</div>";
             }
             $("#weather").html(pageContents);
@@ -50,6 +85,18 @@
             console.log("Complete");
         });
     };
+
+    //Set map options
+    var mapOptions = {
+        zoom: 14,
+        center: {
+            lat: 30.317077,
+            lng: -97.719509
+        }
+    };
+
+    //Render the map
+    var renderMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
     //Call postWeather to populate page
     postWeather(weatherRequest);
